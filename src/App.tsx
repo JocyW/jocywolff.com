03dcd83701c66a8acbdf1ef90263a1b4851fbd59
Jocy Wolff <jocywolff@gmail.com>
2024-05-21 19:04:30 +0200

    chore: remove nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { Bingo } from './pages/bingo/Bingo';
 
 export const App = () => {
   return <div class="md:mx-[1.6cm] flex flex-col md:gap-[1.4cm]">
-    <nav class="bg-dark-green print:hidden flex gap-4 px-6 py-4">
-      <a href="/" class="font-extrabold text-white hover:underline focus:underline">CV</a>
-      <a href="/bingo" class="font-extrabold text-white hover:underline focus:underline">Bingo</a>
-    </nav>
     <Router>
       <Route path="/" component={CV} />
       <Route path="/bingo" component={Bingo} />
